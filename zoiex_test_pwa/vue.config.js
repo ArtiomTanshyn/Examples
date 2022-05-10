d149d53a32d89e00d314cefcb3847fabbfeff4ae
Artem Tanshyn <tanshyn@me.com>
2022-05-10 21:32:25 +0300

    change config

--- zoiex_test_pwa/vue.config.js
+++ zoiex_test_pwa/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
-  publicPath: '/zoiex_test_pwa',
+  publicPath: '/zoiex_test_pwa/',
 })
